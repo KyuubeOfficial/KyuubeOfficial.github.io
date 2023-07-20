f30a24cff5496745ad08df48cb6a56c292a85fdf
GabrielCalhabeu <117700622+KyuubeOfficial@users.noreply.github.com>
2023-07-20 03:59:08 -0300

    pls woek

--- server/src/models/accounts.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-const accountSchema = new mongoose.Schema({
-  user: String,
-  password: String,
-});
-
-module.exports = mongoose.model("account", accountSchema);
--- server/src/models/file.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const fileSchema = new mongoose.Schema({
-  user: String,
-  title: String,
-  file: Array,
-});
-
-module.exports = mongoose.model("savefiles", fileSchema);
